9fdaddf04818325d630eeac3a9e46f5fee871bd6
vagon_straponov <Zelinov_1097@mail.ru>
2020-06-07 20:01:46 +0500

    Updates

---
+++ precache-manifest.6f9fbf446b1562ab2b0435c9b5738cbd.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa982a584a1c4e6d0b369ffc544e783c",
+    "url": "/vetrina-app/index.html"
+  },
+  {
+    "revision": "c3ecc6f9f3e0797f34f2",
+    "url": "/vetrina-app/static/css/2.7a8c10a3.chunk.css"
+  },
+  {
+    "revision": "b5bab35b8997cf13eeec",
+    "url": "/vetrina-app/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "c3ecc6f9f3e0797f34f2",
+    "url": "/vetrina-app/static/js/2.1cb06c5d.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/vetrina-app/static/js/2.1cb06c5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5bab35b8997cf13eeec",
+    "url": "/vetrina-app/static/js/main.c5f4b750.chunk.js"
+  },
+  {
+    "revision": "be5af0fc05d74516b28e",
+    "url": "/vetrina-app/static/js/runtime-main.a20d0fb3.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/vetrina-app/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/vetrina-app/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "a67a271160dcb2e843b184b51623c8eb",
+    "url": "/vetrina-app/static/media/bunker.a67a2711.jpg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/vetrina-app/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/vetrina-app/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/vetrina-app/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/vetrina-app/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/vetrina-app/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/vetrina-app/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/vetrina-app/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/vetrina-app/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/vetrina-app/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/vetrina-app/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/vetrina-app/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/vetrina-app/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/vetrina-app/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/vetrina-app/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/vetrina-app/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "5be244834ac1f2707a7243e9035d0518",
+    "url": "/vetrina-app/static/media/ph14.5be24483.jpeg"
+  },
+  {
+    "revision": "269acad74ad993ebfdd907353b6ac1e1",
+    "url": "/vetrina-app/static/media/ph15.269acad7.jpeg"
+  },
+  {
+    "revision": "1192ab44df98fe53330e6916e4c4e0bb",
+    "url": "/vetrina-app/static/media/ph7.1192ab44.jpeg"
+  },
+  {
+    "revision": "74a7f58ad19405607e7157fb5f90658c",
+    "url": "/vetrina-app/static/media/ph9.74a7f58a.jpeg"
+  }
+]);
